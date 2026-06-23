ceffc69eb74c00f572d4fce226983d39a4a17b48
Vaishnavi Gungone <vaishnavi.sony1576@gmail.com>
2026-06-23 21:27:15 +0530

    frontend URL updated

--- chefbot-backend/main.py
+++ chefbot-backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://chef-bot-pi.vercel.app"
+        "https://chef-bot-oldversion.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
